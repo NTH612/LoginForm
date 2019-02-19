62de2757e5b524e9ff8db59bf2828497297fcd79
LeBinhAn <43633262+LeBinhAn@users.noreply.github.com>
2019-02-19 14:18:52 +0700

    Initial commit

---
+++ LoginForm/DataProcess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace LoginForm
+{
+    public class DataProcess
+    { 
+
+        public SqlConnection GetConnection()
+        {
+          
+           var connect = new SqlConnection("Data Source =LEBINHAN-LAPTOP\\SQLEXPRESS; " +
+               "Initial Catalog = StudentInformation; User ID = sa; Password = thaybadao");
+            return connect;
+        }
+        public bool CheckLogin(string StdName, string StdPass)
+        {
+           string sql = "SELECT * FROM [dbo].[Student] WHERE StdName= @u  AND StdPass=@p";            
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = sql;
+            cmd.Connection = GetConnection();
+            cmd.Parameters.AddWithValue("@u", StdName);
+            cmd.Parameters.AddWithValue("@p", StdPass);
+            cmd.Connection.Open();
+            SqlDataReader rd = cmd.ExecuteReader();
+            bool result = rd.HasRows;
+            cmd.Connection.Close();
+            return result;
+        }
+    }
+}
---
+++ LoginForm/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        DataProcess dt = new DataProcess();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string u = txtUserName.Text;
+            string p = txtPassword.Text;
+            if (dt.CheckLogin(u, p))
+            {
+                MessageBox.Show("OK");
+            }
+         }
+
+        
+    }
+
+
+}
+    
+
+
+
